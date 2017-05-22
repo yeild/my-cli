c56cd7f084d4d961289579d52ae92d561bb69f93
huanglei <798400626@qq.com>
2017-05-22 11:22:55 +0800

    add command

--- index.js
+++ index.js
@@ -3,6 +3,24 @@
 var fs = require('fs');
 var path = require('path');
 
+var config = {};
+process.argv.slice(2).forEach( function (item) {
+  switch (item) {
+    case "-j":
+      config.jquery = true;
+      break;
+    case "-s":
+      config.swiper = true;
+      break;
+    case "-v":
+      config.vue = true;
+      break;
+    case "-b":
+      config.bootstrap = true;
+      break;
+  }
+});
+
 function copyTemplate (from, to) {
   from = path.join(__dirname, 'templates', from);
   write(to, fs.readFileSync(from, 'utf-8'))
@@ -15,33 +33,53 @@ function mkdir (path, fn) {
     fn && fn()
   })
 }
-var PATH = "."
-copyTemplate("index.html", PATH + '/index.html')
-copyTemplate("package.json", PATH + '/package.json')
+
+var PATH = ".";
+
+copyTemplate("index.html", PATH + '/index.html');
+copyTemplate("package.json", PATH + '/package.json');
+
 mkdir(PATH + '/public', function () {
   mkdir(PATH + '/public/css',function () {
-    copyTemplate("css/main.css",PATH + '/public/css/main.css')
-  })
-  mkdir(PATH + '/public/iconfont')
-  mkdir(PATH + '/public/img')
+    copyTemplate("css/main.css",PATH + '/public/css/main.css');
+    config.bootstrap && copyTemplate("css/bootstrap.min.css",PATH + '/public/css/bootstrap.min.css');
+    config.swiper && copyTemplate("css/swiper-3.4.1.min.css",PATH + '/public/css/swiper-3.4.1.min.css')
+  });
+  mkdir(PATH + '/public/iconfont');
+  mkdir(PATH + '/public/img');
   mkdir(PATH + '/public/js',function () {
-    copyTemplate("js/main.js",PATH + '/public/js/main.js')
+    copyTemplate("js/main.js",PATH + '/public/js/main.js');
+    config.bootstrap && copyTemplate("js/bootstrap.min.js",PATH + '/public/js/bootstrap.min.js');
+    config.vue && copyTemplate("js/vue.min.js",PATH + '/public/js/vue.min.js');
+    if(config.jquery) {
+      copyTemplate("js/jquery-1.8.3.min.js",PATH + '/public/js/jquery-1.8.3.min.js');
+      copyTemplate("js/jquery-3.1.1.min.js",PATH + '/public/js/jquery-3.1.1.min.js');
+    }
+    config.bootstrap && copyTemplate("js/bootstrap.min.js",PATH + '/public/js/bootstrap.min.js');
+    if(config.swiper){
+      if(config.jquery) {
+        copyTemplate("js/swiper-3.4.1.jquery.min.js", PATH + '/public/js/swiper-3.4.1.jquery.min.js')
+      } else {
+        copyTemplate("js/swiper-3.4.2.min.js",PATH + '/public/js/swiper-3.4.2.min.js')
+      }
+    }
   })
-})
+});
 mkdir(PATH + '/src', function () {
   mkdir(PATH + '/src/scss',function () {
-    copyTemplate("scss/main.scss",PATH + '/src/scss/main.scss')
+    copyTemplate("scss/main.scss",PATH + '/src/scss/main.scss');
     mkdir(PATH + '/src/scss/base',function () {
-      copyTemplate("scss/base/_common.scss",PATH + '/src/scss/base/_common.scss')
-      copyTemplate("scss/base/_reset.scss",PATH + '/src/scss/base/_reset.scss')
+      copyTemplate("scss/base/_common.scss",PATH + '/src/scss/base/_common.scss');
+      copyTemplate("scss/base/_reset.scss",PATH + '/src/scss/base/_reset.scss');
       copyTemplate("scss/base/_variables.scss",PATH + '/src/scss/base/_variables.scss/')
-    })
-    mkdir(PATH + '/src/scss/component')
-    mkdir(PATH + '/src/scss/helper')
-    mkdir(PATH + '/src/scss/layout')
-    mkdir(PATH + '/src/scss/page')
+    });
+    mkdir(PATH + '/src/scss/component');
+    mkdir(PATH + '/src/scss/helper');
+    mkdir(PATH + '/src/scss/layout');
+    mkdir(PATH + '/src/scss/page');
     mkdir(PATH + '/src/scss/theme')
   })
-})
-console.log("all jobs done.")
-console.log(" use \"npm run sass\" to watch the sass file changes.")
+});
+
+console.log("all jobs done.");
+console.log(" use \"npm run sass\" to watch the sass file changes.");
